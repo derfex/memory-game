3d2cbf473a634a0e30363e1a6c3b36729c7c6aa0
derfex <derfex@yandex.ru>
2024-09-25 12:50:20 +0300

    Refactor (game-card.component.js): Apply the code style

--- src/app/ui-kit/game-card/game-card.component.js
+++ src/app/ui-kit/game-card/game-card.component.js
@@ -1,68 +1,78 @@
-(function() {
+(function () {
   'use strict';
 
-  window.module.GameCardComponent = (function() {
+  window.module.GameCardComponent = (function () {
     class GameCardComponent {
+      #element;
+      #guessed = false;
+      #openCallback = null;
+      #openCardFn = null;
+      #opened = false;
+
       constructor(imageURL) {
-        this.createElement(imageURL)
-        this.imageURL = imageURL; // TODO.
-        this.guessed = false;
-        this.opened = false;
+        this.#createElement(imageURL);
+        this.imageURL = imageURL;
+        this.#guessed = false;
+        this.#opened = false;
       }
 
       // # Public API
       close() {
-        this.element.classList.remove(gameCardCSSClassOpenedModifier);
-        this.opened = false;
-        this.element.textContent = ''; // TODO.
+        this.#element.classList.remove(gameCardCSSClassOpenedModifier);
+        this.#opened = false;
       }
 
       getElement() {
-        return this.element;
+        return this.#element;
       }
 
       markAsGuessed() {
-        this.element.classList.add(gameCardCSSClassGuessedModifier);
-        this.guessed = true;
-        this.element.removeEventListener('click', this.openCardFn);
-        this.openCardFn = null;
+        this.#element.classList.add(gameCardCSSClassGuessedModifier);
+        this.#guessed = true;
+        this.#element.removeEventListener('click', this.#openCardFn);
+        this.#openCardFn = null;
       }
 
       open() {
-        this.element.classList.add(gameCardCSSClassOpenedModifier);
-        this.opened = true;
-        this.element.textContent = this.imageURL; // TODO.
-        if (this.openCallback) {
-          this.openCallback();
+        this.#element.classList.add(gameCardCSSClassOpenedModifier);
+        this.#opened = true;
+        if (this.#openCallback) {
+          this.#openCallback();
         }
       }
 
       subscribeToOpen(openCallback) {
-        this.openCallback = openCallback;
+        this.#openCallback = openCallback;
       }
 
       unsubscribeToOpen() {
-        this.openCallback = null;
+        this.#openCallback = null;
       }
 
       // # Private
-      createElement(imageURL) {
-        const element = this.element = document.createElement('article');
-        element.classList.add(gameCardCSSClass);
-        element.dataset.imageURL = imageURL;
-        this.openCardFn = this.openCard.bind(this);
-        element.addEventListener('click', this.openCardFn);
+      #createElement(imageURL) {
+        const articleElement = this.#element = document.createElement('article');
+        articleElement.classList.add(gameCardCSSClass);
+        this.#openCardFn = this.#openCard.bind(this);
+        articleElement.addEventListener('click', this.#openCardFn);
+
+        const imageElement = document.createElement('div');
+        imageElement.classList.add(gameCardImageCSSClass);
+        imageElement.style.backgroundImage = `url('${imageURL}')`;
+
+        articleElement.appendChild(imageElement);
       }
 
-      openCard() {
+      #openCard() {
         this.open();
       }
     }
 
-    return GameCardComponent
+    return GameCardComponent;
   })();
 
   const gameCardCSSClass = 'app-game-card';
   const gameCardCSSClassGuessedModifier = 'app-js-game-card_guessed';
   const gameCardCSSClassOpenedModifier = 'app-js-game-card_opened';
+  const gameCardImageCSSClass = 'app-game-card__image';
 })();
