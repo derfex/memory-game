928712941d1aaf29ff8e80778da840ada19415be
derfex <derfex@yandex.ru>
2024-09-25 09:46:47 +0300

    Feature (data): Create the images

---
+++ src/data/images/images-list.js
@@ -0,0 +1,22 @@
+(function () {
+  'use strict';
+
+  window.module.ImagesList = (function () {
+    return [
+      'src/data/images/double-glasses.jpg',
+      'src/data/images/horses.jpg',
+      'src/data/images/karelia.jpg',
+      'src/data/images/neva-2.jpg',
+      'src/data/images/neva.jpg',
+      'src/data/images/new-year.jpg',
+      'src/data/images/orkestr.jpg',
+      'src/data/images/pushkin-and-maloy.jpg',
+      'src/data/images/raf.jpg',
+      'src/data/images/rammstein.jpg',
+      'src/data/images/restoran-and-circles.jpg',
+      'src/data/images/sakura.jpg',
+      'src/data/images/vkusnoteevo.jpg',
+      'src/data/images/with-girlfriends.jpg',
+    ];
+  })();
+})();
